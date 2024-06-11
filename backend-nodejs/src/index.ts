fab85091c3adf96679d960196f50b7c42f3b1747
Minh Nguyen Nhat <Minh.NguyenNhat@ebiquity.com>
2024-06-11 14:51:13 +0700

    Setup mongodb

--- backend-nodejs/src/index.ts
+++ backend-nodejs/src/index.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express from "express";
+import { Error } from "mongoose";
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, TypeScript with Express!');
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://minhnn250695:yANNrE03Gcz0ggFI@mindb-nodejs.djpawy2.mongodb.net/?retryWrites=true&w=majority&appName=mindb-nodejs"
+  )
+  .then(() => console.log("Kết nối với MongoDB thành công!"))
+  .catch((err: Error) => console.error("Lỗi kết nối MongoDB:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript with Express!");
 });
 
 app.listen(port, () => {
